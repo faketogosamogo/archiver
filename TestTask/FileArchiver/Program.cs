8af69b8cd5822e8282ca1a12cf4e792295c85342
Ilya <ilya0308199@mail.ru>
2020-11-01 23:09:22 +0700

    самая простая версия, сейчас нужно разделить на потоки

---
+++ TestTask/FileArchiver/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileArchiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             FileCompressor.CompressFile(@"H:\5d1f09e185e17.vid");
+             FileDecompressor.DecompressFile(@"H:\5d1f09e185e17.vid.gz");
+        }
+    }
+}
