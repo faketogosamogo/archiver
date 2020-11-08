fa35dc8044660f36f468d0038d5fbcf1f9a2eca5
Ilya <ilya0308199@mail.ru>
2020-11-08 23:19:23 +0700

    ДА

--- TestTask/FileArchiver/MultithreadFileDecompressor.cs
+++ TestTask/FileArchiver/MultithreadFileDecompressor.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using System.Security.Cryptography;
+using System.Collections.Concurrent;
 
 namespace FileArchiver
 {
@@ -16,72 +17,111 @@ namespace FileArchiver
         private IBlockStreamReader _blockReader;
         private IBlockStreamWriter _blockWriter;
 
-        private static FileStream _outputFile;
         private static FileStream _inputFile;
 
         //количество одновременно запускаемых потоков
-        private static int _threadsCount = 1;
-
-
-        private static object _currentIndexLocker = new object();
+        private static int _threadsCount = 15;
+        private static int _blockLen = 0;
+        private static long _currentWriteIndex = 0;
+        private static object _currentWriteIndexLocker = new object();
         private static object _readLocker = new object();
+        private static bool _isFileClosed;
+        private string _outputFilePath;
 
+        private static byte[] _lastBlock;
         public MultithreadFileDecompressor(IBlockDecompressor blockDecompressor, IBlockStreamWriter blockWriter, IBlockStreamReader blockReader)
         {
             _blockDecompressor = blockDecompressor;
             _blockReader = blockReader;
             _blockWriter = blockWriter;
         }
-
-
-        private void oneThreadBlockOperations()
+        private void ReadBlocks()
         {
-
-            while (true)
+            while (!_isFileClosed)
             {
-                var block = new BlockWithPosition(new byte[0], 0, false);
-                lock (_readLocker)
+                BlockingCollection<BlockWithPosition> blocks = new BlockingCollection<BlockWithPosition>();//Чтобы количество занимаемой памяти приложения не привышало норму
+                for (int i = 0; i < _threadsCount; i++)
                 {
-                    long pos = _outputFile.Position;
-                    
+                  //  lock (_readLocker)
+                   // {
 
-                    var blockPos = new byte[8];
-                    _inputFile.Read(blockPos);
-                    var blockLen = new byte[4];
-                    _inputFile.Read(blockLen);
-                    int nextBlockLen = BitConverter.ToInt32(blockLen);
-                    if (nextBlockLen == 0) return;
+                        if (_inputFile.Position >= _inputFile.Length)
+                        {
+                            _isFileClosed = true;
+                            continue;
+                        }
+                        var blockPosBuf = new byte[8];
+                        _inputFile.Read(blockPosBuf);                        
+                        long blockPos = BitConverter.ToInt64(blockPosBuf);
 
-                    block.Block = _blockReader.ReadBlock(_inputFile, _inputFile.Position, nextBlockLen);
+                        var blockLenBuf = new byte[4];
+                        _inputFile.Read(blockLenBuf);
+                        int blockLen = BitConverter.ToInt32(blockLenBuf);
 
-                    if (block.Block.Length == 0) return;
+                        var block = _blockReader.ReadBlock(_inputFile, _inputFile.Position, blockLen);
+                        if (block.Length == 0) return;
+                        blocks.Add(new BlockWithPosition(block, blockPos, false));
+                    }
 
-
-                    block.Position = BitConverter.ToInt64(blockPos);
-                    block.Block = _blockDecompressor.DecompressBlock(block.Block);
-                }
-                MD5 md = MD5.Create();
-
-                Console.WriteLine($"{block.Position} {block.Block.Length} md: {Convert.ToBase64String(md.ComputeHash(block.Block))}");
-                _blockWriter.WriteBlock(_outputFile, block.Position, block.Block);
-
-
-
+               // }
+                blocks.CompleteAdding();
+                DecompressAndWriteBlocks(blocks);
             }
         }
+        private void DecompressAndWriteBlocks(BlockingCollection<BlockWithPosition> blocks)
+        {
+            foreach (var block in blocks)
+            {              
+                block.Block = _blockDecompressor.DecompressBlock(block.Block);
+                if (block.Block.Length < _blockLen)
+                {
+                    _lastBlock = block.Block;
+                }
+                else
+                {
+                    long pos = 0;
+                    lock (_currentWriteIndexLocker)
+                    {
+                        pos = _currentWriteIndex * _blockLen;
+                        _currentWriteIndex++;
+                    }
+                    MD5 md = MD5.Create();
 
+                    Console.WriteLine(Convert.ToBase64String(md.ComputeHash(block.Block)) + " " + pos);
+
+                    using var outputFile = File.OpenWrite(_outputFilePath);
+                    _blockWriter.WriteBlock(outputFile, block.Position, block.Block);
+                }
+            }
+        }
         public void DecompressFile(string inputFilePath, string outputFilePath)
         {
             _inputFile = File.OpenRead(inputFilePath);
-            _outputFile = File.OpenWrite(outputFilePath);
-            List<Thread> threads = new List<Thread>();
-            for (int i = 0; i < _threadsCount; i++) threads.Add(new Thread(oneThreadBlockOperations));
+            _outputFilePath = outputFilePath;
+            var blockLenBuf = new byte[4];
+            _inputFile.Read(blockLenBuf);
 
-            foreach (var th in threads) th.Start();
-            foreach (var th in threads) th.Join();
-            //oneThreadBlockOperations();
+            _blockLen = BitConverter.ToInt32(blockLenBuf);
+            Console.WriteLine(_blockLen);
+            List<Thread> threads = new List<Thread>();
+            ReadBlocks();
+        
             _inputFile.Dispose();
-            _outputFile.Dispose();
+
+            if (_lastBlock != null)
+            {
+                using (var outputFile = File.OpenWrite(outputFilePath))
+                {
+
+                    outputFile.Position = outputFile.Length;
+                    MD5 md = MD5.Create();
+
+                    Console.WriteLine(Convert.ToBase64String(md.ComputeHash(_lastBlock)) + " " + outputFile.Position);
+
+                    Console.WriteLine(_lastBlock.Length+ " " +  outputFile.Position);
+                    outputFile.Write(_lastBlock);
+                }
+            }
         }
     }
 }
--- TestTask/FileArchiver/MultithreadStreamCompressor.cs
+++ TestTask/FileArchiver/MultithreadStreamCompressor.cs
@@ -13,14 +13,11 @@ namespace FileArchiver
     /// <summary>
     /// Мультипоточная реализация IFileCompressor.
     /// В своей реализации работает поблочно с файлом.
-    /// Считывает->Сжимает->Записывает блок.
-    /// В данной реализации перечисленные выше операции производятся в одном потоке.
-    /// Это было выбрано в связи с тем, что в конечном итоге при использовании нескольких потоков для ускорения сжатия, минимально возможное время сводится к записи в файл,
+    /// (Считывает и Сжимает) блоки -> Записывает блоки.
+    ///Запись в файл происходит постепенно(не мультипоточно).
+    ///Это было выбрано в связи с тем, что в конечном итоге при использовании нескольких потоков для ускорения сжатия, минимально возможное время сводится к записи в файл,
         ///т.к разбивать поблочно файл для записи нецелесообразно в связи с неизвестностью размера сжатых блоков
-        ///(в случае если оставлять пробелы, а после их убирать склеивая файл. Возможно есть и другие способы, но их я не придумал).
-    /// Если разбивать эти операции на разные потоки, то всё равно Считывание + Сжатие занимают больше времени, чем запись и это не даст ускорения(в моём представлении).
-    /// </summary>
-    
+
     class BlockWithPosition
     {
         public string Id { get; set; }
@@ -42,93 +39,94 @@ namespace FileArchiver
         private IBlockStreamWriter _blockWriter;
 
         private static FileStream _outputFile;
-        private static FileStream _inputFile;
 
         //используется для поблочого чтения из файла
-        private static int _currentReadIndex = 0;
-        //длина обрабатываемого блока
-        private static int _blockLen = (1024*1024)*10;
-        //количество одновременно запускаемых потоков
-        private static int _threadsCount = 15;
-
+        private static long _currentReadIndex = 0;
         private static object _currentReadIndexLocker = new object();
-        private static object _writeLocker = new object();
-        private static object _readLocker = new object();
-        private static object _compressLocker = new object();
-        private static string _inputFilePath;
-        private static string _outputFilePath;
-        private static long _writePosition = 0;
 
-        private BlockingCollection<BlockWithPosition> _blocks;
+        //длина обрабатываемого блока
+        private static int _blockLen = (1024 * 1024) * 10;
+        //количество одновременно запускаемых потоков
+        private static int _threadsCount = 5;
+
+
+        private static object _writeLocker = new object();
+        
+        private static string _inputFilePath;  
+        private static bool _isFileClosed;
+
         public MultithreadStreamCompressor(IBlockCompressor blockCompressor, IBlockStreamWriter blockWriter, IBlockStreamReader blockReader)
         {
             _blockCompressor = blockCompressor;
             _blockReader = blockReader;
             _blockWriter = blockWriter;
-            _blocks = new BlockingCollection<BlockWithPosition>();
-        }
-        private void oneThreadBlockOperations()
+        }     
+        private void ReadAndCompressBlocks()
         {
-            
-         
-        }      
-        private void ReadAndCompress()
-        {
-            long pos = 0; 
-            using var inputFile = File.OpenRead(_inputFilePath);
+            while (!_isFileClosed) {//Чтобы потоки не пересоздавались
 
-            lock (_currentReadIndexLocker)
-            {
-                pos = _currentReadIndex * _blockLen;
-                _currentReadIndex++;
-                if (pos >= inputFile.Length) return;
-                Console.WriteLine(pos);
+                BlockingCollection<BlockWithPosition> blocks = new BlockingCollection<BlockWithPosition>();//Чтобы количество занимаемой памяти приложения не привышало норму
+                for (int i = 0; i < _threadsCount; i++)
+                {
+                    long pos = 0;
+                    using var inputFile = File.OpenRead(_inputFilePath);
+
+                    lock (_currentReadIndexLocker)
+                    {
+                        pos = _currentReadIndex * _blockLen;
+                        _currentReadIndex++;
+                        if (pos >= inputFile.Length)
+                        {
+                            _isFileClosed = true;
+                            continue;
+                        }
+                    }
+
+                    var block = _blockReader.ReadBlock(inputFile, pos, _blockLen);
+                    if (block.Length == 0) return;
+                    MD5 md = MD5.Create();
+
+                    Console.WriteLine(Convert.ToBase64String(md.ComputeHash(block)) + " " + pos);
+
+                    block = _blockCompressor.CompressBlock(block);
+                    blocks.Add(new BlockWithPosition(block, pos, true));
+                }
+
+                blocks.CompleteAdding();
+                WriteBlocks(blocks);//После считывания сжатия порции блоков записываем их в файл
             }
-           
-            var block = _blockReader.ReadBlock(inputFile, pos, _blockLen);
-            if (block.Length == 0) return;
-            block = _blockCompressor.CompressBlock(block);
-            _blocks.Add(new BlockWithPosition(block, pos, true));            
         }
-
-        private void Write()
+        private void WriteBlocks(BlockingCollection<BlockWithPosition> blocks)
         {
-            foreach (var block in _blocks)
-            {
+           foreach (var block in blocks)
+           {
                 var blockWithPosLen = new byte[8 + 4 + block.Block.Length];
                 BitConverter.GetBytes(block.Position).CopyTo(blockWithPosLen, 0);
                 BitConverter.GetBytes(block.Block.Length).CopyTo(blockWithPosLen, 8);
+
                 block.Block.CopyTo(blockWithPosLen, 12);
                 lock (_writeLocker)
                 {
                     _blockWriter.WriteBlock(_outputFile, _outputFile.Position, blockWithPosLen);
                 }
-            }
+           }
         }
        
         public void CompressFile(string inputFilePath, string outputFilePath)
         {
             _inputFilePath = inputFilePath;
-            _outputFilePath = outputFilePath;
-           // _inputFile = File.OpenRead(inputFilePath);
             _outputFile = File.OpenWrite(outputFilePath);
-           // oneThreadBlockOperations();
+            Console.WriteLine(_blockLen);
+
+            _outputFile.Write(BitConverter.GetBytes(_blockLen));
+
             List<Thread> threads = new List<Thread>();
-            List<Thread> readThreads = new List<Thread>();
 
-            for (int i = 0; i < _threadsCount; i++) threads.Add(new Thread(Write));
-            for (int i = 0; i < _threadsCount; i++) readThreads.Add(new Thread(ReadAndCompress));
-
-            foreach (var readTh in readThreads) readTh.Start();
-            foreach (var readTh in readThreads) readTh.Join();
-            _blocks.CompleteAdding();
-
-            Write();
-           // foreach (var th in threads) th.Start();
-
-           // foreach (var th in threads) th.Join();
+            for (int i = 0; i < _threadsCount; i++) threads.Add(new Thread(ReadAndCompressBlocks));
            
-           // _inputFile.Dispose();       
+            foreach (var th in threads) th.Start(); 
+            foreach (var th in threads) th.Join();
+        
             _outputFile.Dispose();       
         }
     }
